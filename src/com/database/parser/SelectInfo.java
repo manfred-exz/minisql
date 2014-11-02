c0129856f2f3658f29aaa45f35b3d4ee0d7f106c
manfred-exz <manfred.exz@gmail.com>
2014-11-02 21:50:07 +0800

    complete the 'select' clause and 'from' clause, 'where' clause not finished.

--- src/com/database/parser/SelectInfo.java
+++ src/com/database/parser/SelectInfo.java
@@ -6,20 +6,17 @@ import java.util.ArrayList;
  * Created by manfred on 11/2/14.
  */
 public class SelectInfo extends QueryInfo {
-	public ArrayList<String> tableList = null;
-	public ArrayList<Attribute> selectAttributes = null;   // If it's empty, we should return all the attributes in the specified table.
+	public String [] tableList = null;
+	public Attribute [] selectAttributes = null;   // If it's empty, we should return all the attributes in the specified table.
 	//TODO: public ArrayList<Condition> conditionList;
 
 	//TODO: used a different way of parsing from CreateTableInfo, should unify them later.
 	public SelectInfo(ArrayList<String> queryElementIn) throws Exception {
 		super(QueryType.SELECT);
 
-		// Initialization
-		tableList = new ArrayList<String>();
-		selectAttributes = new ArrayList<Attribute>();
-
 		String [] queryElement = new String[queryElementIn.size()];
 		queryElement = (String[])queryElementIn.toArray(queryElement);
+
 
 		// Check if the passed in query is right.
 		if(queryElement[0].equals("select"))
@@ -27,8 +24,12 @@ public class SelectInfo extends QueryInfo {
 		else
 			throw new Exception(SelectInfo.class + ": " +  "It's not a 'Select' query, should not be passed in.");
 
+		// seek to the 'From' part, and check the find all the tables used in the query.
+		if( (tableList = parseFromTable(queryElement, 0)) == null)
+			throw new Exception(SelectInfo.class + "[SelectInfo]: " +  "Not From clause found.");
 
-
+		// seek to the 'Select' part and check if any attributes specified.
+		selectAttributes = parseSelectAttributes(queryElement, 0);
 	}
 
 }
