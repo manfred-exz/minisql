a4ee7d1612fa834284891fb23f503c933891458b
manfred-exz <manfred.exz@gmail.com>
2014-11-04 14:54:10 +0800

    use attribute to store constant to support the condition like table.attr > constant

--- src/com/database/parser/Attribute.java
+++ src/com/database/parser/Attribute.java
@@ -3,10 +3,17 @@ package com.database.parser;
 /**
  * Created by manfred on 10/29/14.
  */
+
+/*
+* To simplify the storage of conditionNode,
+* Attributes can be used to store forms of "{table_name.}attribute_name", or "constant"
+* Thus, before using any attribute, you should check if the type is what you want.
+* */
 public class Attribute {
 	public final static String INT_TYPE = "int";
 	public final static String CHAR_TYPE = "char";
 	public final static String FLOAT_TYPE = "float";
+	public final static String CONSTANT_TYPE = "constant";
 
 	//TODO: table name not added into constructor.
 	// tableName = null means: which table it belong to is not specified.
@@ -14,25 +21,40 @@ public class Attribute {
 	private String attributeName = null;
 	private String attributeType = null;
 	private int attributeLength = 0;
+	private double constant = 0;
 	private boolean isUnique = false;
 
-	public Attribute(String newAttributeName, String newTableName){
-		attributeName = newAttributeName;
-		tableName = newTableName;
+	public Attribute(String _attributeName, String _tableName){
+		attributeName = _attributeName;
+		tableName = _tableName;
 	}
 
-	public Attribute(String newAttributeName, String newAttributeType,int newAttributeLength, boolean newIsUnique){
-		attributeName = newAttributeName;
-		attributeType = newAttributeType;
-		attributeLength = newAttributeLength;
-		isUnique = newIsUnique;
+	public Attribute(String _attributeType, double _constant){
+		attributeType = _attributeType;
+		constant = _constant;
+
 	}
 
-	public Attribute(String newAttributeName, String newAttributeType, int newAttributeLength){
-		attributeName = newAttributeName;
-		attributeType = newAttributeType;
-		attributeLength = newAttributeLength;
+	public Attribute(String _attributeName, String _attributeType,int _attributeLength, boolean _isUnique){
+		attributeName = _attributeName;
+		attributeType = _attributeType;
+		attributeLength = _attributeLength;
+		this.isUnique = _isUnique;
+	}
+
+	public Attribute(String _attributeName, String _attributeType, int _attributeLength){
+		attributeName = _attributeName;
+		attributeType = _attributeType;
+		attributeLength = _attributeLength;
 		isUnique = false;
+	}
+
+	public double getConstant(){
+		return constant;
+	}
+
+	public void setConstant(double _constant){
+		constant = _constant;
 	}
 
 	public String getAttributeName(){
